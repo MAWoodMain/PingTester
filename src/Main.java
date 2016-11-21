d18086d7b94e1f1b6898be681b9047aff7f6157c
Matthew Wood <mattcow12@googlemail.com>
2016-11-21 15:18:14 +0000

    Added ipv6 testing in the main loops, added CSV headers and -1 fillers for missing data

--- src/Main.java
+++ src/Main.java
@@ -15,12 +15,21 @@ public class Main
         FileOutputStream fos = new FileOutputStream(args[1]);
         PrintWriter pw = new PrintWriter(fos);
 
+        pw.println(",,IPV4,,,,IPV6,,,");
+        System.out.println(",,IPV4,,,,IPV6,,,");
+        pw.println("Rank,Hostname,min,avg,max,mdev,min,avg,max,mdev");
+        System.out.println("Rank,Hostname,min,avg,max,mdev,min,avg,max,mdev");
+
         String line;
         while ((line = br.readLine()) != null)
         {
-            line = line + "," +
+            line += "," +
                     Arrays.toString(
-                            new PingTester(PingTester.Mode.IPV4, line.split(",")[1]).run())
+                            new PingTester(PingTester.Mode.IPV4, "www." + line.split(",")[1]).run())
+                            .replace("[", "").replace("]", "").replace(" ", "");
+            line += "," +
+                    Arrays.toString(
+                            new PingTester(PingTester.Mode.IPV6, "www." + line.split(",")[1]).run())
                             .replace("[", "").replace("]", "").replace(" ", "");
             pw.println(line);
             System.out.println(line);
--- src/PingTester.java
+++ src/PingTester.java
@@ -1,13 +1,12 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 /**
  * PingTester - PACKAGE_NAME
  * Created by matthew on 19/11/16.
  */
-public class PingTester
+class PingTester
 {
     enum Mode
     {
@@ -27,6 +26,8 @@ public class PingTester
 
     float[] run()
     {
+
+        float[] data = {-1f,-1f,-1f,-1f};
         String cmd = mode == Mode.IPV4? "ping" : "ping6";
         cmd += ARGUMENTS;
         cmd += hostname;
@@ -41,18 +42,17 @@ public class PingTester
             }
 
             String[] parts = output.split("= "); // split data from preamble
-            if (parts.length < 2) return null;
+            if (parts.length < 2) return data;
             output = parts[1];
             output = output.substring(0,output.indexOf("ms")); // remove end junk
 
             parts = output.split("/"); // split on data separator
-            if (parts.length < 4) return null; // check length
+            if (parts.length < 4) return data; // check length
 
-            float[] data = new float[4];
             for (int i = 0; i<4; i++) data[i] = Float.parseFloat(parts[i]); // convert data
             return data;
         } catch (IOException e) {
-            return null;
+            return data;
         }
 
     }
